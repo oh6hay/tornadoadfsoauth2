75b3b83fe84439961d41847f6c5c3470ec0c5797
Osku <ossi.vaananen@sanoma.com>
2021-04-09 16:24:45 +0300

    added project url

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(
     name='tornadoadfsoauth2',
-    version='0.0.1',
+    version='0.0.3',
     description='ADFS Oauth2 authentication for tornadoweb',
     author='Ossi Vaananen',
     author_email='ossi.vaananen@me.com',
     packages=['tornadoadfsoauth2'],
-    install_requires=['tornadoweb', 'pyjwt'],
+    url = 'https://github.com/oh6hay/tornadoadfsoauth2',
+    install_requires=['tornadoweb', 'pyjwt', 'cryptography'],
     license='BSD 3-clause')
